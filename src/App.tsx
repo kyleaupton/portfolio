be2a0173c053b15dd6f3744bec2e822c37af4316
Kyle Upton <kyleaupton@gmail.com>
2022-12-28 10:53:51 -0600

    Scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
 import './App.css';
+
+function Layout() {
+  return (
+    <div className="layout">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="app">
-      <header className="app-header" />
-      <div className="app-content" />
+      {/* Define the routes here...? React is weird */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="home" element={<div />} />
+          <Route path="about" element={<div />} />
+          <Route path="dashboard" element={<div />} />
+          {/* 404 */}
+          <Route path="*" element={<div />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
