90cd9c904ee99fc258a69a215acb05fc11e87811
Kyle Upton <kyleaupton@gmail.com>
2023-09-20 19:23:58 -0400

    better tech

---
+++ src/components/icons/items/Python.vue
@@ -0,0 +1,13 @@
+<template>
+  <img src="../../../assets/python.png">
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Python'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
