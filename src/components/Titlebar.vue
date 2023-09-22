280a23d5e6eea098d3a786c9e46cf73b7300e6db
Kyle Upton <kyleaupton@gmail.com>
2023-09-22 19:27:26 -0400

    added email and toasts system

--- src/components/Titlebar.vue
+++ src/components/Titlebar.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="titlebar">
     <ClickableIcon
+      v-tooltip="{ content: 'Copy Email', delay: 800 }"
+      icon="envelope"
+      @click="copyEmail"
+    />
+
+    <ClickableIcon
       v-tooltip="{ content: 'Open LinkedIn', delay: 800 }"
       icon="linkedin"
       @click="openLink('https://linkedin.com/in/kyle-upton-50bb1a188')"
@@ -16,6 +22,8 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { mapActions } from 'pinia';
+import { useToastsStore } from '../stores/toasts';
 import ClickableIcon from './icons/ClickableIcon.vue';
 
 export default defineComponent({
@@ -26,6 +34,13 @@ export default defineComponent({
   },
 
   methods: {
+    ...mapActions(useToastsStore, ['showToast']),
+
+    copyEmail() {
+      navigator.clipboard.writeText('kyleaupton@gmail.com');
+      this.showToast({ message: 'Email Copied to Clipboard' });
+    },
+
     openLink(link: string) {
       window.open(link);
     },
