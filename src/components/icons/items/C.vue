9a524cc6e8ea459b7f37925764b70e35b422664f
Kyle Upton <kyleaupton@gmail.com>
2024-04-02 00:34:53 -0400

    added node-wimlib-split

---
+++ src/components/icons/items/C.vue
@@ -0,0 +1,13 @@
+<template>
+  <img src="../../../assets/c.png" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'C',
+});
+</script>
+
+<style scoped></style>
