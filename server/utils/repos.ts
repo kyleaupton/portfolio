63081a774d588ad0d5582e348fc6722d354f5c19
Kyle Upton <kyleaupton@gmail.com>
2024-04-14 23:44:09 -0400

    data fetching adds

--- server/utils/repos.ts
+++ server/utils/repos.ts
@@ -1,15 +1,19 @@
-import { Octokit } from "octokit";
-
 /**
  * Module that exports utility functions for fetching data about my GitHub repos
  */
 
-// List of repos to fetch
-const repos: {
+import { Octokit } from "octokit";
+
+const REFRESH_INTERVAL = 1000 * 60 * 15; // 15 minutes
+
+type PersistedRepo = {
   id: string;
   icons?: string[];
   npm?: string;
-}[] = [
+};
+
+// List of repos to fetch
+const repos: PersistedRepo[] = [
   {
     id: "boot-builder",
     icons: ["vue", "electron", "typescript"],
@@ -67,67 +71,18 @@ export const getRepoData = async () => {
   return cache;
 };
 
-const refreshData = async () => {
+const fetchAll = async () => {
   console.log("utils/repos.ts: Refreshing repo data");
 
-  for (const repo of repos) {
-    try {
-      console.log(`utils/repo.ts: Fetching data for ${repo.id}`);
-      const start = Date.now();
-
-      // Main repo data
-      const { data: repoData } = await octokit.rest.repos.get({
-        owner: "kyleaupton",
-        repo: repo.id,
-      });
-
-      // Commits
-      // Bit of a hack here, we request `listCommits` with 1 commit per page
-      // then count the total number of pages to get the total number of commits
-      const { headers } = await octokit.rest.repos.listCommits({
-        owner: "kyleaupton",
-        repo: repo.id,
-        per_page: 1,
-        page: 1,
-      });
-
-      let commits = 0;
-      const paginationLink = headers.link;
-      if (paginationLink) {
-        const lastPageMatch = paginationLink.match(/page=(\d+)>; rel="last"/);
-        if (lastPageMatch) {
-          commits = parseInt(lastPageMatch[1]);
-        }
-      }
-
-      // Readme
-      const { data: readmeData } = await octokit.rest.repos.getReadme({
-        owner: "kyleaupton",
-        repo: repo.id,
-      });
-
-      const readme = Buffer.from(readmeData.content, "base64").toString(
-        "utf-8"
-      );
-
-      console.log(
-        `utils/repo.ts: Adding ${repo.id} to cache. Took ${
-          Date.now() - start
-        }ms.`
-      );
-
-      cache[repo.id] = {
-        data: repoData,
-        commits,
-        readme,
-        icons: repo.icons,
-        npm: repo.npm,
-      };
-    } catch (e) {
-      console.error(`Failed to fetch repo data for ${repo.id}`);
-      console.error(e);
-    }
-  }
+  await Promise.race([
+    new Promise((_, reject) =>
+      setTimeout(
+        () => reject(new Error("fetchAll() timed out")),
+        REFRESH_INTERVAL
+      )
+    ),
+    Promise.all(repos.map(fetchRepo)),
+  ]);
 
   console.log(
     `utils/repos.ts: Finished refreshing repo data. ${
@@ -136,11 +91,66 @@ const refreshData = async () => {
   );
 };
 
+const fetchRepo = async (repo: PersistedRepo) => {
+  try {
+    console.log(`utils/repo.ts: Fetching data for ${repo.id}`);
+    const start = Date.now();
+
+    // Main repo data
+    const { data: repoData } = await octokit.rest.repos.get({
+      owner: "kyleaupton",
+      repo: repo.id,
+    });
+
+    // Commits
+    // Bit of a hack here, we request `listCommits` with 1 commit per page
+    // then count the total number of pages to get the total number of commits
+    const { headers } = await octokit.rest.repos.listCommits({
+      owner: "kyleaupton",
+      repo: repo.id,
+      per_page: 1,
+      page: 1,
+    });
+
+    let commits = 0;
+    const paginationLink = headers.link;
+    if (paginationLink) {
+      const lastPageMatch = paginationLink.match(/page=(\d+)>; rel="last"/);
+      if (lastPageMatch) {
+        commits = parseInt(lastPageMatch[1]);
+      }
+    }
+
+    // Readme
+    const { data: readmeData } = await octokit.rest.repos.getReadme({
+      owner: "kyleaupton",
+      repo: repo.id,
+    });
+
+    const readme = Buffer.from(readmeData.content, "base64").toString("utf-8");
+
+    console.log(
+      `utils/repo.ts: Adding ${repo.id} to cache. Took ${Date.now() - start}ms.`
+    );
+
+    cache[repo.id] = {
+      data: repoData,
+      commits,
+      readme,
+      icons: repo.icons,
+      npm: repo.npm,
+    };
+  } catch (e) {
+    console.error(`Failed to fetch repo data for ${repo.id}`);
+    console.error(e);
+  }
+};
+
 // Upon module load, fetch the data
-refreshData();
+fetchAll();
 
 // Refresh the data every 15 minutes
-setInterval(refreshData, 1000 * 60 * 15);
+setInterval(fetchAll, REFRESH_INTERVAL);
 
 // Types
 type Repo = {
