6735738f7b0c537ab6b8c0b3408c7d16e57d791c
Kyle Upton <kyleaupton@gmail.com>
2024-04-21 13:33:54 -0400

    added google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,10 +14,19 @@ export default defineNuxtConfig({
   build: {
     transpile: ["@fortawesome/vue-fontawesome"],
   },
-  modules: ["@nuxtjs/color-mode", "shadcn-nuxt", "floating-vue/nuxt"],
+  modules: [
+    "@nuxtjs/color-mode",
+    "shadcn-nuxt",
+    "floating-vue/nuxt",
+    "nuxt-gtag",
+  ],
   colorMode: {
     preference: "dark",
     fallback: "dark",
     classSuffix: "",
+  },
+  // @ts-ignore
+  gtag: {
+    id: "G-W1E8B7RHGN",
   },
 });
