622ad9577444eccbb8ddd5e346531be1216b8011
Kyle Upton <kyleaupton@gmail.com>
2024-09-02 18:20:48 -0400

    titlebar ui improvements

--- components/Titlebar.vue
+++ components/Titlebar.vue
@@ -1,112 +1,121 @@
 <template>
   <div
-    class="titlebar flex justify-between items-center bg-neutral-900 mb-8 border px-4 py-4 sm:px-8"
+    class="titlebar flex justify-center items-center mb-8 border px-4 py-4 sm:px-8"
   >
-    <a href="/">
-      <fa-icon
-        class="fa-2x fa-spin"
-        style="--fa-animation-duration: 7s"
-        :icon="['fa-solid', 'fa-gear']"
-      />
-    </a>
+    <div class="flex justify-between items-center w-full max-w-screen-xl">
+      <a href="/">
+        <fa-icon
+          class="fa-2x"
+          style="--fa-animation-duration: 7s"
+          :icon="['fa-solid', 'fa-gear']"
+        />
+      </a>
 
-    <div class="flex justify-center gap-2">
-      <template v-for="icon in icons" :key="icon.key[1]">
-        <component
-          :is="isLinkIcon(icon) ? 'a' : 'span'"
-          :href="isLinkIcon(icon) ? icon.link : undefined"
-          target="_blank"
-          @click="handleClick(icon)"
+      <div class="flex justify-center gap-2">
+        <template
+          v-for="icon in icons"
+          :key="icon.key[1]"
         >
-          <Button
-            v-tooltip="{
-              content: icon.tooltip,
-            }"
-            variant="outline"
-            size="icon"
+          <component
+            :is="isLinkIcon(icon) ? 'a' : 'span'"
+            :href="isLinkIcon(icon) ? icon.link : undefined"
+            target="_blank"
+            @click="handleClick(icon)"
           >
-            <fa-icon class="fa-xl" :icon="icon.key" />
-          </Button>
-        </component>
-      </template>
+            <Button
+              v-tooltip="{
+                content: icon.tooltip,
+              }"
+              variant="outline"
+              size="icon"
+            >
+              <fa-icon
+                class="fa-xl"
+                :icon="icon.key"
+              />
+            </Button>
+          </component>
+        </template>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { useToast } from "@/components/ui/toast/use-toast";
+import { defineComponent } from 'vue'
+import { useToast } from '@/components/ui/toast/use-toast'
 
 type IconBase = {
-  key: [string, string];
-  tooltip?: string;
-};
+  key: [string, string]
+  tooltip?: string
+}
 
 type IconLink = IconBase & {
-  link: string;
-};
+  link: string
+}
 
 type IconClick = IconBase & {
-  onClick: () => void;
-};
+  onClick: () => void
+}
 
-type Icon = IconLink | IconClick;
+type Icon = IconLink | IconClick
 
 export default defineComponent({
-  name: "Titlebar",
+  name: 'Titlebar',
 
   computed: {
     icons(): Icon[] {
       return [
         {
-          key: ["fas", "id-card"],
-          link: "https://kyleupton.dev/resume",
-          tooltip: "Open Resume",
+          key: ['fas', 'id-card'],
+          link: 'https://kyleupton.dev/resume',
+          tooltip: 'Open Resume',
         },
         {
-          key: ["fas", "envelope"],
+          key: ['fas', 'envelope'],
           onClick: this.copyEmail,
-          tooltip: "Copy Email",
+          tooltip: 'Copy Email',
         },
         {
-          key: ["fab", "linkedin"],
-          link: "https://linkedin.com/in/kyle-upton-50bb1a188",
-          tooltip: "Open LinkedIn",
+          key: ['fab', 'linkedin'],
+          link: 'https://linkedin.com/in/kyle-upton-50bb1a188',
+          tooltip: 'Open LinkedIn',
         },
         {
-          key: ["fab", "github"],
-          link: "https://github.com/kyleaupton",
-          tooltip: "Open GitHub",
+          key: ['fab', 'github'],
+          link: 'https://github.com/kyleaupton',
+          tooltip: 'Open GitHub',
         },
-      ];
+      ]
     },
   },
 
   methods: {
     async copyEmail() {
-      const { toast } = useToast();
+      const { toast } = useToast()
 
       try {
-        await navigator.clipboard.writeText("kyleaupton@gmail.com");
-        toast({ title: "Email Copied to Clipboard" });
-      } catch (e) {
-        toast({ title: "Failed to Copy Email", variant: "destructive" });
+        await navigator.clipboard.writeText('kyleaupton@gmail.com')
+        toast({ title: 'Email Copied to Clipboard' })
+      }
+      catch (e) {
+        toast({ title: 'Failed to Copy Email', variant: 'destructive' })
       }
     },
 
     handleClick(icon: Icon) {
       if (this.isClickIcon(icon)) {
-        icon.onClick();
+        icon.onClick()
       }
     },
 
     isLinkIcon(icon: Icon): icon is IconLink {
-      return (icon as IconLink).link !== undefined;
+      return (icon as IconLink).link !== undefined
     },
 
     isClickIcon(icon: Icon): icon is IconClick {
-      return (icon as IconClick).onClick !== undefined;
+      return (icon as IconClick).onClick !== undefined
     },
   },
-});
+})
 </script>
